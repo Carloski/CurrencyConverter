3b9c98cd9024815abd7b436475be6738d8139f3f
carloski <carlomtamayo@gmail.com>
2020-05-29 11:29:00 -0400

    Changed image and primary color. Corrected fetch bug and added swap functionality to the currencies (swap button).

--- app.js
+++ app.js
@@ -10,13 +10,18 @@ const swap = document.getElementById('swap');
 function calculate() {
   const currency_one = currencyElement_one.value;
   const currency_two = currencyElement_two.value;
-  const proxyurl = 'https://cors-anywhere.herokuapp.com/';
-  let url = `https://api.exchangeratesapi.io/latest?base=${currency_one}`;
+  let url = `https://api.exchangerate-api.com/v4/latest/${currency_one}`;
 
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch(() => console.log("can't access response. Blocked by browser?"));
+    .then((data) => {
+      // console.log(data);
+      const rate = data.rates[currency_two].toFixed(2);
+
+      rateElement.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountElement_two.value = (amountElement_one.value * rate).toFixed(2);
+    });
 }
 
 // Event Listeners
@@ -24,5 +29,13 @@ currencyElement_one.addEventListener('change', calculate);
 amountElement_one.addEventListener('input', calculate);
 currencyElement_two.addEventListener('change', calculate);
 amountElement_two.addEventListener('input', calculate);
+
+// event listener to swap currencies when the swap button is pressed
+swap.addEventListener('click', (e) => {
+  const temp = currencyElement_one.value;
+  currencyElement_one.value = currencyElement_two.value;
+  currencyElement_two.value = temp;
+  calculate();
+});
 
 calculate();
